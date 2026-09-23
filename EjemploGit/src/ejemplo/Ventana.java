53311471a2212ffb47802a30bead4c2ae63f560c
Samuel Correa M <samuel.correa@opendeusto.es>
2026-09-23 09:33:34 +0200

    Cambio de nombre titulo

--- EjemploGit/src/ejemplo/Ventana.java
+++ EjemploGit/src/ejemplo/Ventana.java
@@ -9,11 +9,11 @@ public class Ventana extends JFrame {
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(320,420);
-		setTitle("Prueba Git");
+		setTitle("Ventanita");
 		
 		setVisible(true);
 	}
 	public static void main(String[] args) {
-		
+		new Ventana();
 	}
 }
